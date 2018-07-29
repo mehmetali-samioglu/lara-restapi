3ada85a1c2a6abc3c9d9076417fbfa93b92ff052
sqltimer@gmail.com <Sqlzamani1357>
2018-07-29 17:19:37 +0300

    Product detayları productresource ile dönüştürüldü.

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ProductResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
+     * Kaynağı bir diziye dönüştürün.
+     * Özel dizi ile api olşuturuyoruz
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
@@ -19,8 +20,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1-($this->discount/100))*$this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),1):'Henüz Değerlendirme Yok.',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
